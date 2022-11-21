060ec77903f93663478bfce62163496d826c2b29
Vanessa O <vaneolipa@gmail.com>
2022-11-21 16:00:57 -0500

    Ceate a js dile with the variables

---
+++ src/modules/variables.js
@@ -0,0 +1,4 @@
+export const shopCart = document.querySelector('.cart-modal-container');
+export const shopContainer = document.querySelector('#table-body');
+export const clearShopCart = document.querySelector('.cart-modal-button');
+export const productsList = document.querySelector('.products-container');
