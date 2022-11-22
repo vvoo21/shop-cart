639c86a399bcb98554e13d21b384bd78b792a2fc
Vanessa O <vaneolipa@gmail.com>
2022-11-22 14:35:41 -0500

    Create single page application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 import {
-  productsList, shopCart, openShopCart, shopCartModal, menuModal, menuIcon, menuClose,
-} from './modules/variables.js';
+  productsList, shopCart, openShopCart, shopCartModal, menuModal, menuIcon, menuClose, home, products, about, contact, homeLink, productsLink, aboutLink, contactLink, menuHome, menuProducts, menuAbout, menuContact, links} from './modules/variables.js';
 import addProduct from './modules/addProduct.js';
 import { removeProduct } from './modules/functions.js';
 
@@ -24,4 +23,61 @@ menuIcon.addEventListener('click', () => {
 // close menu
 menuClose.addEventListener('click', () => {
   menuModal.classList.remove('show');
+});
+
+links.forEach((link) => { 
+  link.addEventListener('click', () => {
+    menuModal.classList.remove('show');
+  });
+});
+
+//navigate to the different sections
+homeLink.addEventListener('click', () => {
+  home.style.display = 'block';
+  products.style.display = 'block';
+  about.style.display = 'block';
+  contact.style.display = 'none';
+});
+productsLink.addEventListener('click', () => {
+  home.style.display = 'none';
+  products.style.display = 'block';
+  about.style.display = 'none';
+  contact.style.display = 'none';
+});
+aboutLink.addEventListener('click', () => {
+  home.style.display = 'none';
+  products.style.display = 'none';
+  about.style.display = 'block';
+  contact.style.display = 'none';
+});
+contactLink.addEventListener('click', () => {
+  home.style.display = 'none';
+  products.style.display = 'none';
+  about.style.display = 'none';
+  contact.style.display = 'block';
+});
+
+menuHome.addEventListener('click', () => {
+  home.style.display = 'block';
+  products.style.display = 'block';
+  about.style.display = 'block';
+  contact.style.display = 'none';
+});
+menuProducts.addEventListener('click', () => {
+  home.style.display = 'none';
+  products.style.display = 'block';
+  about.style.display = 'none';
+  contact.style.display = 'none';
+});
+menuAbout.addEventListener('click', () => {
+  home.style.display = 'none';
+  products.style.display = 'none';
+  about.style.display = 'block';
+  contact.style.display = 'none';
+});
+menuContact.addEventListener('click', () => {
+  home.style.display = 'none';
+  products.style.display = 'none';
+  about.style.display = 'none';
+  contact.style.display = 'block';
 });
